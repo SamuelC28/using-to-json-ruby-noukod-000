3e34ac75404a817d7fc748cd90ee0b26a2969d5c
Samuel Cornet <samuel@noukod.com>
2019-03-07 17:02:37 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,9 +28,10 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [ author: { only: [:name]}])
-  end
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
 
 private
   # Use callbacks to share common setup or constraints between actions.
